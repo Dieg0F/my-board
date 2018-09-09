821a2b33faf8a911b37b7c9f3f8f274fdc86d5ae
Dieg0F <dfraga2012@gmail.com>
2018-09-09 14:22:21 -0300

    Plugins e configurações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 import { HeaderColor } from "@ionic-native/header-color";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage = 'LoginPage';
+  rootPage = "LoginPage";
 
-  constructor(platform: Platform, splashScreen: SplashScreen, statusBar: StatusBar, headerColor: HeaderColor) {
+  constructor(
+    platform: Platform,
+    splashScreen: SplashScreen,
+    statusBar: StatusBar,
+    headerColor: HeaderColor
+  ) {
     headerColor.tint("#F44336");
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.backgroundColorByHexString('FFFFFF');
+      statusBar.backgroundColorByHexString("FFFFFF");
       splashScreen.hide();
     });
   }
 }
-
